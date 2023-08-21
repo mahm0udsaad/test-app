55c5ed6d5be2310428cde4e67ad679b10063aa59
mahm0udsaad <saad123mn123@gmail.com>
2023-08-21 05:08:21 +0200

    last Stage

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,18 +3,18 @@ import usePopup from './hooks/usePopup';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook, faCalculator, faFlask, faHistory } from '@fortawesome/free-solid-svg-icons';
-
+import { motion } from 'framer-motion';
 const Home = () => {
     const { PopUp: EnglishPopup } = usePopup({ path: 'English' ,icon:faBook  ,color:''});
     const { PopUp: MathPopup } = usePopup({ path: 'Math' ,icon:faCalculator  ,color:'orange'});
     const { PopUp: SciencePopup } = usePopup({ path: 'Science' ,icon:faFlask ,color:'green' });
-    const { PopUp: HistoryPopup } = usePopup({ path: 'Math' ,icon:faHistory  ,color:'red'});
-  
+    const { PopUp: HistoryPopup } = usePopup({ path: 'History' ,icon:faHistory  ,color:'red'});
+
   return (
     <div
       className="main font-Font2 h-screen flex flex-col justify-center items-center"
     >
-      <h1 className="lg:text-4xl text-2xl  mb-5 text-center text-white font-semibold ">Welcome to the Test App</h1>
+      <h1 className="animate-charcter lg:text-4xl text-2xl  mb-5 text-center text-white font-semibold ">Welcome to the Test App</h1>
       <p className="text-lg mb-8">Get ready to test your knowledge!</p>
       <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
        <MathPopup />
